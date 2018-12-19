93406d369980a0132088d2697c69855a5bf8df79
Holden Altaffer <haltaffer@gmail.com>
2018-12-19 20:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function produceDrivingRange(f) {
-  return f;
+function createDriver() {
+  let DriverId = 0;
+  // return the class
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++DriverId;
+    }
+
+    produceDrivingRange() {
+      
+    }
+  };
 }
