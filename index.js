076384e67a7d5f59bc7b76c736131a70f86af6de
Holden Altaffer <haltaffer@gmail.com>
2018-12-19 20:36:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function createDriver() {
+  let DriverId = 0;
+  // return the class
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++DriverId;
+    }
+
+
+  };
+}
+
+function produceDrivingRange(blockRange) {
+  return function(start, end) {
+    let blocks = parseInt(end.split('th')[0]) - parseInt(start.split('th')[0]);
+    if (blocks <= blockRange) {
+      return `within range by ${blockRange - blocks}`;
+    } else {
+      return `${blocks - blockRange} blocks out of range`;
+    }
+  };
+}
+
+function produceTipCalculator(calculator) {
+  return function(bill) {
+    return bill * calculator;
+  }
+}
