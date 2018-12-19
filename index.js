a9321ec1bd6dbb89bf2cfc1c275b236a236abda9
Holden Altaffer <haltaffer@gmail.com>
2018-12-19 20:25:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function createDriver() {
 }
 
 function produceDrivingRange(blockRange) {
-  return function(start, end) {
-    let blocks = parseInt(end.split('th')[0]) - parseInt(start.split('th')[0]);
-    if (blocks <= blockRange) {
-      return `within range by ${blockRange - blocks}`;
-    } else {
-      return `${blocks - blockRange}` blocks out of range`;
-    }
-  };
-}
+//   return function(start, end) {
+//     let blocks = parseInt(end.split('th')[0]) - parseInt(start.split('th')[0]);
+//     if (blocks <= blockRange) {
+//       return `within range by ${blockRange - blocks}`;
+//     } else {
+//       return `${blocks - blockRange}` blocks out of range`;
+//     }
+//   };
+// }
 }
